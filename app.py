cc0995c08ef62773020cffba85d7b302f2cddf2a
Als1510 <lekhrajsharmaas@gmail.com>
2022-10-07 13:42:28 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from click import option
 import nltk
 import pickle
 import string
+nltk.download('punkt')
+nltk.download('stopwords')
 import streamlit as st
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 ps = PorterStemmer()
 
 st.set_page_config(page_title='SMS / Email Spam Classifier', layout = 'centered', page_icon = None, initial_sidebar_state = 'expanded')
@@ -45,7 +48,7 @@ if try_dummies:
     'Select Message',
     ("Congratulations! You've won a $1000 xyz gift card. Go to http://xyz to claim now.", "Your IRS tax refund is pending acceptance. Must accept within 24 hours: http://xyz", "URGENT Your grandson was arrested last night as Mexico. Need bail money immediately Western Unione Wire $9,500 http://xyz","Today am going to college so am not able to attend the class", "Do one thing! Chnage the sentence into: 'Because i want to concentrate in my educational career i'm living here..'", "I walked an hour to see you! Doesn't that show I care about you."))
   
-st.title("Email/SMS Spam Classifier")
+st.title("SMS/Email Spam Classifier")
 input = st.text_area("Enter the message", value=selected)
 if st.button("Predict"):
   transformed_input = transform_text(input)
